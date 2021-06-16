9064f926de416b5eaf31bb5f924e5092a01a6ff7
NavSous <53831933+NavSous@users.noreply.github.com>
2021-06-16 08:39:10 -0700

    Commit 0

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const wiki = require("wikijs").default
+//Below, insert your bot login token as login_token
+let login_token = "";
+//Below insert your bot user id as bot_id
+let bot_id = ""
+client.once('ready', ()=> {
+    console.log("Bot activated");
+})
+client.on("message", (msg) => {
+    let message = msg.content
+    if(message.includes("./") && msg.author.id != bot_id){
+        let x = message.split("/")
+        const res = x[1].replace(" ", "_")
+        let lnk = "https://en.wikipedia.org/wiki/"+res
+        msg.channel.send(lnk)       
+    }
+    if(message.includes(".!info")){
+        msg.channel.send("Welcome to WikiBot! This bot allows you to get the wikipedia article to anything you type in discord. Use './title of article' to search now!")       
+    }
+})
+client.login(login_token)
